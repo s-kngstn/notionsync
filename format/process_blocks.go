edb946f0da2b701ed83c738ec10119ca9d2fb01c
s-kngstn <s.kngstn@protonmail.com>
2024-03-08 11:49:11 +0000

    fix: recursively call blocks so we get all child blocks

--- format/process_blocks.go
+++ format/process_blocks.go
@@ -7,43 +7,59 @@ import (
 	"github.com/s-kngstn/notionsync/api"
 )
 
-func ProcessBlocks(uuid string, results *api.ResultsWrapper, outputPath string, pageName string, apiClient *api.NotionApiClient, bearerToken string, processedBlocks map[string]string) {
-	// We need to handle if we have a child page block has children and if so, we need to call the API again
-	// The ID of the child page is the UUID of the block
+func ProcessBlocks(uuid string, results *api.ResultsWrapper, outputPath, pageName string, apiClient *api.NotionApiClient, bearerToken string, processedBlocks map[string]string) {
 	for _, block := range results.Results {
-		// Assume every block has a unique ID that can be used as a key in the map
-		blockID := block.ID
-
-		if _, processed := processedBlocks[blockID]; processed {
+		if _, processed := processedBlocks[block.ID]; processed {
 			// If the block has already been processed, skip it or handle as needed
 			continue
 		}
 
 		if block.Type == "child_page" && block.HasChildren {
-			// Call the API with the ID of the block
-			childResults, err := apiClient.GetNotionBlocks(block.ID, bearerToken)
-			if err != nil {
-				fmt.Println("Error calling API:", err)
-				return
-			}
-			// The output path will be the same as the parent page, but with the title of the child page name
-			childPageName := strcase.ToKebab(block.ChildPage.Title)
-			childOutputPath := fmt.Sprintf("output/%s.md", childPageName)
-
-			// Before processing child blocks, mark this block as processed to avoid infinite recursion
-			processedBlocks[blockID] = childOutputPath
-
-			// Write the child blocks to a Markdown file with the title of the child pageName
-			if err := WriteBlocksToMarkdown(childResults, childOutputPath, childPageName); err != nil {
-				fmt.Println("Error writing blocks to Markdown:", err)
+			processChildBlocks(&block, apiClient, bearerToken, processedBlocks)
+		} else if block.Type == "link_to_page" {
+			print("LINK TO PAGE")
+			// Handle link_to_page blocks similarly if they require recursive processing
+			if _, processed := processedBlocks[block.LinkToPage.PageID]; processed {
+				fmt.Println("LTP PROCCESSED BEFORE")
+				// I need to link to the file that was created for that page
+			} else {
+				fmt.Println("LTP NOT YET PROCCESSED")
+				// I need to processChildBlocks here
 			}
 		}
+
+		// After processing children, mark the current block as processed
+		processedBlocks[block.ID] = outputPath
 	}
 
-	// Mark the parent block as processed using the original UUID and the output path
-	processedBlocks[uuid] = outputPath
-	// Now process and write the results to a Markdown file
+	// After all blocks are processed, write the current page's blocks to markdown
 	if err := WriteBlocksToMarkdown(results, outputPath, pageName); err != nil {
 		fmt.Println("Error writing blocks to Markdown:", err)
+	}
+}
+
+func processChildBlocks(parentBlock *api.Block, apiClient *api.NotionApiClient, bearerToken string, processedBlocks map[string]string) {
+	childResults, err := apiClient.GetNotionChildBlocks(parentBlock.ID, bearerToken)
+	if err != nil {
+		fmt.Println("Error calling API for child blocks:", err)
+		return
+	}
+	childPageName := strcase.ToKebab(parentBlock.ChildPage.Title)
+	childOutputPath := fmt.Sprintf("output/%s.md", childPageName)
+
+	// Before processing child blocks further, mark this parent block as processed to avoid infinite recursion
+	processedBlocks[parentBlock.ID] = childOutputPath
+
+	// Write the child blocks to a Markdown file
+	if err := WriteBlocksToMarkdown(childResults, childOutputPath, childPageName); err != nil {
+		fmt.Println("Error writing blocks to Markdown:", err)
+		return
+	}
+
+	// If child blocks have their own children, recursively process them too
+	for _, childBlock := range childResults.Results {
+		if childBlock.HasChildren {
+			processChildBlocks(&childBlock, apiClient, bearerToken, processedBlocks)
+		}
 	}
 }

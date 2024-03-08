21d0c38dd49a1c63fb8ca3f455e773d503db9037
s-kngstn <s.kngstn@protonmail.com>
2024-03-08 22:14:44 +0000

    initialise user input

--- cmd/notionsync/main.go
+++ cmd/notionsync/main.go
@@ -1,34 +1,60 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"net/http"
 	"os"
 	"sync"
 
+	"github.com/joho/godotenv"
 	"github.com/s-kngstn/notionsync/api"
 	"github.com/s-kngstn/notionsync/format"
 	"github.com/s-kngstn/notionsync/pkg/cli"
 	"github.com/s-kngstn/notionsync/pkg/fetch"
-	"github.com/s-kngstn/notionsync/pkg/token"
 	"github.com/s-kngstn/notionsync/pkg/utils"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file or no .env file found:", err)
+	}
+
+	appEnv := os.Getenv("APP_ENV")
+
+	tokenFlag := flag.String("token", "", "Notion API bearer token")
 	filePath := flag.String("file", "", "Path to the file containing URLs")
 	flag.Parse()
 
-	userInput := cli.RealUserInput{}
-	tokenValue := cli.Prompt(userInput, "Please enter the Notion API bearer token: ")
-	token.PersistToken(tokenValue)
+	if appEnv == "development" {
+		if err := godotenv.Load(); err != nil {
+			fmt.Println("No .env file found")
+		}
+	}
+
+	bearerToken := os.Getenv("NOTION_API_KEY")
+
+	// If in production and token not provided through flag, prompt for it
+	if appEnv != "development" && *tokenFlag == "" {
+		// Initialize RealUserInput with os.Stdin
+		if bearerToken == "" {
+			inputReader := bufio.NewReader(os.Stdin)
+			userInput := cli.NewRealUserInput(inputReader)
+			bearerToken = cli.Prompt(userInput, "Please enter the Notion API bearer token: ")
+		}
+	} else if *tokenFlag != "" {
+		// If token is provided through flag, use it
+		bearerToken = *tokenFlag
+	}
 
 	var urls []string
-	var err error
 
 	if *filePath == "" {
 		// No file path provided, prompt for a single URL
-		userInput := cli.RealUserInput{}
+		inputReader := bufio.NewReader(os.Stdin)
+		userInput := cli.NewRealUserInput(inputReader)
 		url := cli.Prompt(userInput, "Please enter the Notion page URL: ")
 		urls = append(urls, url)
 	} else {
@@ -42,7 +68,6 @@ func main() {
 
 	client := &http.Client{}
 	apiClient := api.NewNotionApiClient(client)
-	bearerToken := os.Getenv("NOTION_BEARER_TOKEN")
 
 	var wg sync.WaitGroup // WaitGroup to wait for all goroutines to finish
 	// This mutal exclusion is for safely updating the processedBlocks map
